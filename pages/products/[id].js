f3aafb37e74a19a1c2a0b8aeab47e0d499990663
guitarlanguage <guitarlanguageseries@gmail.com>
2022-10-14 21:09:57 -0500

    created a shared fetchJson function, implemented fallback blocking

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -8,15 +8,20 @@ export async function getStaticPaths() {
     paths: products.map((product) => ({
       params: { id: product.id.toString() },
     })),
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
 export async function getStaticProps({ params: { id } }) {
-  const product = await getProduct(id);
-  return {
-    props: { product },
-  };
+  try {
+    const product = await getProduct(id);
+    return {
+      props: { product },
+      revalidate: 30, // seconds
+    };
+  } catch (err) {
+    return { notFound: true };
+  }
 }
 
 function ProductPage({ product }) {
